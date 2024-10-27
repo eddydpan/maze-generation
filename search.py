a45512694cf83a70cf9308ef526fe1656872044b
eddydpan <eddydotpan@gmail.com>
2024-10-27 19:45:11 -0400

    Write docstrings and update search.py

--- search.py
+++ search.py
@@ -9,14 +9,28 @@ class Search(ABC):
     An abstract class that BFS and DFS inherit from.
     """
     @abstractmethod
-    def __init__(self, graph=None, start_node=None):
+    def __init__(self, graph=None, start_node='1'):
         self.graph = graph
         self.start = start_node
 
     @abstractmethod
     def generate_spanning_tree(self, dim):
+        """
+        Generates a spanning tree.
+
+        Args:
+            dim: a tuple of integers (w, h) where w is the width of the
+                graph, and h is the height.
+        """
         pass
     
     @abstractmethod
-    def solve(self):
+    def solve(self, target):
+        """
+        Finds the shortest path of the tree that represents the maze using BFS.
+        
+        Args:
+            target: A String of an int representing the target node to find the
+                shortest path to.
+        """
         pass
