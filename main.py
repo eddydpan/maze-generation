0dd169a687a7fb7d5f438c01b28ef09c1247780f
Cong Hoang Le <cle@olin.edu>
2024-10-27 19:37:53 -0400

    Adjacency Matrix function in Maze Class

--- main.py
+++ main.py
@@ -14,23 +14,37 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 maze_width = 20
 maze_height = 20
 maze = Maze(maze_width, maze_height)
+print(maze.adjacency_matrix())
 
 # create rect representation of maze object
 maze_rect = []
 padding = 20
+line_width = 3
 pygame_maze_width = SCREEN_WIDTH - 2 * padding
 pygame_maze_height = SCREEN_HEIGHT - 2 * padding
 cell_width = pygame_maze_width/maze_width
 cell_height = pygame_maze_height/maze_height
-for x in range(maze_width):
-    for y in range(maze_height):
-        maze_rect.append(pygame.Rect(padding + (cell_width * x), padding + (cell_height * y), cell_width, cell_height))
+
+def display_maze():
+    for row in range(maze_width):
+        for col in range(maze_height):
+            cell = maze.grid[row][col]
+            cur_cell_x = padding + cell_width*col
+            cur_cell_y = padding + cell_height*row
+            if cell.walls[0]:
+                pygame.draw.line(screen, (29, 94, 138), (cur_cell_x, cur_cell_y), (cur_cell_x + cell_width, cur_cell_y), line_width)
+            if cell.walls[1]:
+                pygame.draw.line(screen, (29, 94, 138), (cur_cell_x + cell_width, cur_cell_y), (cur_cell_x + cell_width, cur_cell_y + cell_height), line_width)
+            if cell.walls[2]:
+                pygame.draw.line(screen, (29, 94, 138), (cur_cell_x, cur_cell_y + cell_height), (cur_cell_x + cell_width, cur_cell_y + cell_height), line_width)
+            if cell.walls[3]:
+                pygame.draw.line(screen, (29, 94, 138), (cur_cell_x, cur_cell_y), (cur_cell_x, cur_cell_y + cell_height), line_width)
 
 # main loop for pygame
 run = True
 while run:
-    for i in maze_rect:
-        pygame.draw.rect(screen, (29, 94, 138), i, 1)
+    display_maze()
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
--- maze.py
+++ maze.py
@@ -10,16 +10,44 @@ class Cell:
         self.visited = False
         self.solution = False
 
+    def remove_wall(side):
+        self.walls[side] = False
+
 class Maze:
     def __init__(self, width, height):
-        self.cur_row = [[Cell(j, i) for j in range(width)] for i in range(height)]
+        self.grid = [[Cell(j+1, i+1) for j in range(width)] for i in range(height)]
         self.width = width
         self.height = height
 
     def __str__(self):
         cur_string = ""
-        for i in self.cur_row:
+        for i in self.grid:
             for j in i:
                 cur_string = cur_string + "| "
             cur_string = cur_string + "|\n"
         return cur_string
+
+    def get_cell(self, x, y):
+        return self.grid[y-1][x-1]
+
+    def adjacency_matrix(self):
+        def coordinates_to_node(x, y):
+            return (y-1) * self.height + x
+        adjacency_matrix = {}
+        for row in self.grid:
+            for cell in row:
+                adjacent = []
+                if cell.y != 1:
+                    up_cell = self.get_cell(cell.x, cell.y-1)
+                    adjacent.append(coordinates_to_node(up_cell.x, up_cell.y))
+                if cell.x != self.width:
+                    right_cell = self.get_cell(cell.x+1, cell.y)
+                    adjacent.append(coordinates_to_node(right_cell.x, right_cell.y))
+                if cell.y != self.height:
+                    bottom_cell = self.get_cell(cell.x, cell.y+1)
+                    adjacent.append(coordinates_to_node(bottom_cell.x, bottom_cell.y))
+                if cell.x != 1:
+                    left_cell = self.get_cell(cell.x-1, cell.y)
+                    adjacent.append(coordinates_to_node(left_cell.x, left_cell.y))
+                adjacency_matrix[str(coordinates_to_node(cell.x, cell.y))] = adjacent
+        return adjacency_matrix
